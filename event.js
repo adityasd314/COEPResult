4e0d11ed317b2370ea218ac9a1ed97a2b0f1d92f
Aditya Deshmukh <141867706+adityasd314@users.noreply.github.com>
2023-12-23 01:39:33 +0530

    Update event.js

--- event.js
+++ event.js
@@ -4,13 +4,7 @@ let inputBox = document.querySelector('.input-box'),
     closeIcon = document.querySelector('.close-icon');
 
 // ---- ---- Open Input ---- ---- //
-searchIcon.addEventListener('click', () => {
-    inputBox.classList.add('open');
-});
-// ---- ---- Close Input ---- ---- //
-closeIcon.addEventListener('click', () => {
-    inputBox.classList.remove('open');
-});
+
 const findResult = (text) => {
 
     try {
