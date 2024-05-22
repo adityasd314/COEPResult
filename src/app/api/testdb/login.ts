35190087a423c7830a7e67a705742549efe69359
adityasd314 <adityasd314@gmail.com>
2024-05-22 21:24:18 +0530

    minor bug fixes

--- src/app/api/testdb/login.ts
+++
@@ -1,8 +0,0 @@
-import supabase
-const { data, error } = await supabase.auth.signInWithOtp({
-    email: 'example@email.com',
-    options: {
-      // set this to false if you do not want the user to be automatically signed up
-      shouldCreateUser: false,
-    },
-  })
--- src/app/api/testdb/route.ts
+++
@@ -1,8 +0,0 @@
-import clientPromise from "@/lib/mongodb";
-export async function GET() {
-    const client = await clientPromise;
-    if (client) {
-        return Response.json({ status: "connected to db" });
-    }
-    return Response.json({ status: "not connected to db" });
-}
